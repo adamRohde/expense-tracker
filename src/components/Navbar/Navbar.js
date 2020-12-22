1438a6b4cd6f7bed04bdf4de6ea6d2d9c114063a
Adam Rohde <adamrohde@outlook.com>
2020-12-21 23:45:12 -0600

    added react-bootstrap

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
-import { MenuItems } from "./MenuItems";
 import Hamburger from "./Hamburger";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 <link
     src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
@@ -11,13 +12,33 @@ import Hamburger from "./Hamburger";
 
 //
 
-class Navbar extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-expand-xl">
-                <Hamburger />
-            </nav>
-        );
-    }
-}
+const Navbar = () => {
+    const updateNavBar = () => {
+        console.log("updateNavBar");
+        setNavBar();
+    };
+
+    const [navBar, setNavBar] = useState("");
+    return (
+        <nav className="navbar navbar-expand-xl">
+            <div class="dropdown">
+                <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </DropdownButton>
+            </div>
+            <Hamburger />
+        </nav>
+    );
+};
+
 export default Navbar;
+
+{
+    /* <div className="burger">
+<div className="burger-line"></div>
+<div className="burger-line"></div>
+<div className="burger-line"></div>
+</div> */
+}
